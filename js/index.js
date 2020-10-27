a67ac1e759dacff6e5d623a5810586fcd5ba9a08
ruizaj13 <ruizaj13@gmail.com>
2020-10-27 15:09:54 -0700

    six events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// Your code goes here
+
+
+const nav = document.querySelector('.nav')
+
+nav.addEventListener('mouseover', event => {
+    event.preventDefault();
+    event.target.style.color = '#FFEBCD';
+    event.target.style.fontSize = '125%';
+        
+    nav.addEventListener('mouseout', event=> {
+        event.target.style.color = 'initial';
+        event.target.style.fontSize = 'initial';
+    })
+})
+
+nav.addEventListener('mousedown', event =>{
+    event.target.style.border = '1px dashed black'
+
+    nav.addEventListener('mouseup', event => {
+        event.target.style.border = 'none'
+    })
+})
+
+
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('dblclick', event => {
+    logo.style.color = 'red';
+    logo.textContent = 'I DONT LIKE THAT';
+
+    logo.addEventListener('dblclick', event => {
+        logo.style.color = 'initial';
+        logo.textContent = 'Fun Bus';
+  
+    })
+})
+
+document.addEventListener('copy', event => {
+    alert('COPYING IS NOT ALLOWED');
+})
+
+
